23f7d33fcfc686d653e063103eabfb58e824bc12
Daniel Osojnik <zabooz1988@gmai.com>
2024-05-24 09:07:53 +0200

    added read status

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,18 @@ class Book {
   title: string;
   author: string;
   pages: number;
+  read: string
 
   constructor(
     title: string,
     author: string,
     pages: string,
+    read: boolean,
   ) {
     this.title = title.trim() 
     this.author = author.trim() 
     this.pages = +pages
+    this.read =  read ? 'Finished' : 'Not Finished'
   }
 }
 
@@ -41,26 +44,42 @@ class Library{
         this.library.forEach(book => {
 
           const div = document.createElement('div')
-          div.className ='book',
+          div.className ='book'
 
           div.innerHTML=`
-          <h2>${book.title}</h2>
+          <h3>${book.title}</h3>
           <p>${book.author}</p>
           <p>${book.pages}</p>
+          <p class="readStatus">${book.read}</p>
           <button class="removeBtn">Remove</button>
           `;
-          main.append(div)
-          const removeBtn = div.querySelector<HTMLButtonElement>(".removeBtn")
-          
-          if(removeBtn){
 
-            removeBtn.addEventListener('click', () => {
-            
+          const removeBtn = div.querySelector<HTMLButtonElement>(".removeBtn")
+      
+            removeBtn?.addEventListener('click', () => {
               this.removeBook(book.title)
               this.displayBooks()
             })
-          }
+     
+
+          const readStatus = div.querySelector<HTMLParagraphElement>(".readStatus")
+
+          readStatus?.addEventListener('click',()=> {
+            readStatus.innerHTML = readStatus.innerHTML === 'Finished' ?'Not Finished' :'Finished'
+            book.read = readStatus.innerHTML
+            div.classList.toggle("bookRead")
+          })
+
+
+          main.append(div)
         })
+
+        const addBtn = document.createElement('button')
+        addBtn.id = 'addBtn'
+        addBtn.innerHTML ='+'
+
+        main.append(addBtn)
+
       }
     }
 
@@ -90,11 +109,11 @@ const author =
   document.querySelector<HTMLInputElement>("#author")?.value ||
   "Unknown Author";
 const pages = document.querySelector<HTMLInputElement>("#pages")?.value || "0";
-    
+const read = document.querySelector<HTMLInputElement>('#read')?.checked  || false
 
    
 
-      const book = new Book(title,author,pages)
+      const book = new Book(title,author,pages,read)
   
       library.addBook(book)
       library.displayBooks()
